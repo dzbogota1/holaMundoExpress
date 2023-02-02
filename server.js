ff2394cd670f4633a5b6ae3e4df6deaec511e3c2
dzbogota1 <dzbogota1@gmail.com>
2023-02-02 12:18:42 -0500

    aa

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080;
+const port = /*process.env.PORT ||*/ 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hola, mundo horroroso 1!')
+    res.send('Hola, mundo horroroso 1! ' + process.env.PORT)
 })
 
 app.get('/hola', (req, res) => {
-    res.send('Hola, mundo horroroso 2!')
+    res.send('Hola, mundo horroroso 2! ' + process.env.PORT)
 })
 
 app.listen(port, () => {
